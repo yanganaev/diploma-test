8c99fe865a69d3c1881824bf32b7985966f59637
Felix Yanganaev <yangfell@gmail.com>
2022-02-02 15:33:38 +0300

    edit files

--- azure/main.tf
+++ azure/main.tf
@@ -11,7 +11,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstate6501"
+    storage_account_name = "tfstate26811"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
